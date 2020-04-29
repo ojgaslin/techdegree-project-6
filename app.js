454a98df7a9ebbae01b1994987e54781dbca9062
ogaslin <olivia.gaslin@gmail.com>
2020-04-28 22:02:32 -0400

    app.js file added
    server, routes, middleware in progress

--- app.js
+++ app.js
@@ -1,1 +1,27 @@
 const express = require('express');
+
+const path = require('path');
+
+const app = express();
+
+app.set('view engine', 'pug');
+
+const data = require('data.json');
+
+app.use(express.static('public'));
+
+app.get('/index', (req, res) => {
+ response.send('test');
+ res.locals = data.projects;
+ res.render('Home');
+});
+
+app.get('/about', (req, res) => {
+ response.send('test');
+ res.render('about');
+});
+
+
+app.listen(3000, () => {
+  console.log('App is listening to port 3000')
+});
