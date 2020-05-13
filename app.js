05de81129ec5d8349ad702aac46575f9c4811ec5
ogaslin <olivia.gaslin@gmail.com>
2020-05-13 14:07:52 -0400

    error fixes

--- app.js
+++ app.js
@@ -2,44 +2,31 @@ const express = require('express');
 const projects = require('./data.json');
 const path = require('path');
 const bodyParser = require('body-parser');
-const cookieParser = require('cookie-parser');
+//const cookieParser = require('cookie-parser');
 //Init app
 const app = express();
 
 app.use(bodyParser.urlencoded({extended: false}));
-app.use(cookieParser());
+//app.use(cookieParser());
 app.use(express.static('public'));
 //load view engine
 app.set('view engine', 'pug');
 
-app.use((req, res, next) => {
-  console.log("Hello");
-  const err = new Error();
-  err.status= 500;
-  next(err);
-});
+// app.use((req, res, next) => {
+//   console.log("Hello");
+//   const err = new Error();
+//   err.status= 500;
+//   next(err);
+// });
 
 app.use((req, res, next) => {
-   console.log('world');
    next();
 })
-
-// app.use((req, res, next) => {
-//   const err = new Error('Not Found');
-//   err.status = 404;
-//   next(err);
-// })
-
-// app.use((err, req, res, next) => {
-// res.locals.error = err;
-// res.status(err.status);
-// res.render('error');
-// });
 
 //index page route
 app.get('/index', (req, res, next) => {
  res.render('index', { projects });
- res.locals = data.projects;
+ res.locals = projects;
 
 });
 
